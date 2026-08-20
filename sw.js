288edc9a34197830cfe5e81de174e741e6f792d1
Tactical Grid MGRS <azimut01@gmx.fr>
2026-08-20 15:42:41 +0200

    Upgrade service worker to v2 and enhance caching
    Updated service worker to version 2 and improved caching logic.

--- sw.js
+++ sw.js
@@ -1,13 +1,26 @@
-const cacheName = 'mgrs-france-v1';
-const assets = ['index.html', 'icon.png'];
+const cacheName = 'mgrs-france-v2';
+const assets = ['index.html'];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets))));
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+    e.waitUntil(clients.claim());
+});
 
 self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(response => response || fetch(e.request).then(res => {
-        if(e.request.url.includes('tile.openstreetmap.org')) {
-            caches.open(cacheName).then(cache => cache.put(e.request, res.clone()));
-        }
-        return res;
-    })));
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request).then(res => {
+                if (e.request.url.includes('tile.openstreetmap.org')) {
+                    caches.open(cacheName).then(cache => {
+                        cache.put(e.request, res.clone());
+                    });
+                }
+                return res;
+            }).catch(() => {});
+        })
+    );
 });
